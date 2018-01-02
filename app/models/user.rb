81360e86bae3431128a6a3bfe7420a796c01c3ff
TatsuyaKamohara <kamontia@gmail.com>
2018-01-02 17:54:54 +0900

    Update links between OAuth properties and model schema.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
-# class User < ApplicationRecord
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+# class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
       user.uid = auth['uid']
       user.name = auth['info']['name']
-      user.screen_name = auth['info']['nickname']
+      user.nickname = auth['info']['nickname']
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180101122202) do
+ActiveRecord::Schema.define(version: 20180102084800) do
 
   create_table "favorites", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180101122202) do
     t.string "provider"
     t.string "uid"
     t.string "nickname"
+    t.string "name"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
